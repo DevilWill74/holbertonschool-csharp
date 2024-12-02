61f6775362b5ef17263a8358fc0b96c7f1618816
root <Alexis.Fauqueur@outlook.fr>
2024-12-02 10:49:12 +0000

    test

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-
-# Nom du projet et du dossier
-PROJECT_NAME="1-new_project"
-
-# Vérification si le dossier existe déjà
-if [ -d "$PROJECT_NAME" ]; then
-    echo "Le dossier $PROJECT_NAME existe déjà. Suppression et création d'un nouveau projet."
-    rm -rf "$PROJECT_NAME"
-fi
-
-# Création du projet console
-echo "Création d'un nouveau projet C# dans le dossier $PROJECT_NAME..."
-dotnet new console -o "$PROJECT_NAME"
-
-# Construction du projet
-echo "Construction du projet..."
-cd "$PROJECT_NAME" || exit
-dotnet build
-
-echo "Le projet $PROJECT_NAME a été créé et construit avec succès."
+#!/usr/bin/env bash
+dotnet new console --output 1-new_project
+dotnet build 1-new_project
