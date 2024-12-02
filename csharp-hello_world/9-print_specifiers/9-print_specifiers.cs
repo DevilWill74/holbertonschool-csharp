3aee33f0dc03dd7572c4688d273b21e7c6fdee9f
root <Alexis.Fauqueur@outlook.fr>
2024-12-02 12:43:41 +0000

    test

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿ï»¿using System;
 class Program
 {
 	static void Main(string[] args)
@@ -7,6 +6,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:0.00%}", percent);
-        Console.WriteLine("Currency: {0:C2}", currency);
+		Console.WriteLine("Currency: {0:C}", currency);
 	}
 }
