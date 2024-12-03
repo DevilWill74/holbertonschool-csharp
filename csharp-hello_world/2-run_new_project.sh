8549bbac2e4f0899ddb9ad8af3034fd2e666c64a
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-03 11:06:53 +0100

    test

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-print_integer
-dotnet build 2-print_integer
-dotnet run --project -print_integer
+dotnet new console --output 2-new_project
+dotnet build 2-new_project
+dotnet run --project ./2-new_project
