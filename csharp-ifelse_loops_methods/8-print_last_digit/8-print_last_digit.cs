ffa4d10f4d3cbbe95d1d85f37e5bc6d0d6d8f57b
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-03 12:20:48 +0100

    updatefinal

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 
 class Number
 {
     public static int PrintLastDigit(int number)
     {
-        int ud = number%10;
-        if (ud < 0)
-            ud = ud * -1;
-        Console.Write(ud);
-        return ud;
+        int n = number % 10;
+        if (n < 0)
+            n = n * (-1);
+        Console.Write(n);
+        return n;
     }
 }
