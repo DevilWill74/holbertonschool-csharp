9d1cd9cb0cdb2c6afa8a41423bf651c6d248dac6
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-03 12:23:22 +0100

    test

--- csharp-ifelse_loops_methods/8-print_last_digit/obj/Debug/net8.0/8-print_last_digit.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/obj/Debug/net8.0/8-print_last_digit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857fa1a070fc4c98e4c10639b78582bac2a6ea20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa4d10f4d3cbbe95d1d85f37e5bc6d0d6d8f57b")]
 [assembly: System.Reflection.AssemblyProductAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
