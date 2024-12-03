126cf0070b85e174adfc615327f07aab4d64a7ac
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-03 12:29:24 +0100

    test

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+            Console.WriteLine("{0}", new string('_', length));
+        else
+            Console.WriteLine();
+    }
+}
