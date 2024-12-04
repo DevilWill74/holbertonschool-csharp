ee4d141a7ba277170560b6fe9311e66f514ab461
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-04 14:54:34 +0100

    test

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        double percent = .7553;
-        double currency = 98765.4321;
-        Console.WriteLine($"Percent: {percent.ToString("P2", CultureInfo.InvariantCulture)}");
-        Console.WriteLine($"Currency: {currency.ToString("C2", CultureInfo.GetCultureInfo("en-US"))}");
+        double percent = .7553; double currency = 98765.4321;
+        CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+        Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine("Currency: {0:C2}", currency);
     }
 }
