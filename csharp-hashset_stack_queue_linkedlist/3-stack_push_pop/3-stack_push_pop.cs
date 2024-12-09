2eb8a0cd2e19908f4d470e464a329fc0a20d3c3a
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-09 13:09:39 +0100

    test

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int Nitems = aStack.Count;
+        Console.WriteLine("Number of items: {0}", Nitems);
+        if (Nitems <= 0)
+            Console.WriteLine("Stack is empty");
+        else  
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        
+        if (!aStack.Contains(search))
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+        else
+        {
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aStack.Contains(search))
+                aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
