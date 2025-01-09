d06af08e0534339cd79605dda8bd20e01243f7fe
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-10 00:55:39 +0100

    test

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>public class that defines a zombie.</summary>
+    public class Zombie {
+        /// <summary>public int that defines the health of a zombie.</summary>
+        public int health;
+
+        /// <summary>public zombie with health value 0.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>public zombie with health value as a variable.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
