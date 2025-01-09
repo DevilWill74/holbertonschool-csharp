973ff5001e76a478639b02c91ac733801f1689f4
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-10 00:53:09 +0100

    test

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>public class that defines a zombie.</summary>
+    public class Zombie {
+        /// <summary>public int that defines the health of a zombie.</summary>
+        public int health;
+
+        /// <summary>public zombie with health value 0.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
