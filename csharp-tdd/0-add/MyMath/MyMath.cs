367615249001759346f33f6ca382d963aba5df94
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-10 01:04:20 +0100

    test

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Class Operations.</summary>
+    public class Operations
+    {
+    /// <summary>Method that adds two numbers.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            int len = nums.Count;
+            if (len == 0 || nums == null)
+                return 0;
+            nums.Sort();
+            return nums[len -1];
+        }
+    }
+}
