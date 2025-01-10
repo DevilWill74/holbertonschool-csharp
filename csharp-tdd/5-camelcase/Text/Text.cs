d0fd3b1421b3c3384ddfe6b2ad3c3c9fea2f015c
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-10 11:10:38 +0100

    test

--- csharp-tdd/5-camelcase/Text/Text.cs
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿﻿using System;
 
 namespace Text
 {
-    public class Class1
+    ///<summary>Str Class</summary>
+    public class Str
     {
+        ///<summary>CamelCase method: determines how many words are in a string.</summary>
+        public static int CamelCase(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            int wordCount = 1;
+            foreach (char letter in s)
+            {
+                if (Char.IsUpper(letter))
+                {
+                    wordCount += 1;
+                }
+            }
+            return wordCount;
+        }
     }
 }
