096e6ea65e06ae2081d40cb46fba352cfb826c75
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-27 18:19:25 +0100

    update1.0

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,32 +1,49 @@
-﻿using System;
+using System;
 
 ///<summary>MatrixMath class</summary>
 class MatrixMath
 {
-    ///<summary>method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
-    // resource : https://math.stackexchange.com/questions/1404484/how-is-that-a-rotation-by-an-angle-%CE%B8-about-the-origin-can-be-represented-by-this
+    ///<summary>
+    /// Rotates a square 2D matrix by a given angle in radians and returns the resulting matrix.
+    ///</summary>
+    /// <param name="matrix">A 2x2 matrix to rotate.</param>
+    /// <param name="angle">The rotation angle in radians.</param>
+    /// <returns>
+    /// The resulting matrix after rotation, or a matrix containing -1 if the input is invalid.
+    /// </returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double[,] defaultMatrix = new double[,] {{-1}};
-        double[,] twoDmatrix = new double [2,2];
-        double[,] rotate = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}}; 
-        int r = matrix.GetLength(0);
-        int c = matrix.GetLength(1);
-
-        if (r != 2 || c != 2)
+        // Validation : Vérifie si la matrice est 2x2
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
         {
-           return defaultMatrix;
+            return new double[,] { { -1 } };
         }
-        for (int i = 0; i < r; i++)
+
+        // Matrice de rotation
+        double cosTheta = Math.Cos(angle);
+        double sinTheta = Math.Sin(angle);
+        double[,] rotationMatrix = new double[2, 2]
         {
-            for (int j = 0; j < c; j++)
+            { cosTheta, -sinTheta },
+            { sinTheta, cosTheta }
+        };
+
+        // Résultat
+        double[,] result = new double[2, 2];
+
+        // Appliquer la rotation
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
             {
-                for (int k = 0; k < 2; k++)
-                {
-                    twoDmatrix[i, j] = Math.Round(twoDmatrix[i, j] + (matrix[i, k] * rotate[k, j]), 2);
-                }
+                result[i, j] = Math.Round(
+                    matrix[i, 0] * rotationMatrix[0, j] +
+                    matrix[i, 1] * rotationMatrix[1, j],
+                    2
+                );
             }
         }
-        return twoDmatrix;
+
+        return result;
     }
 }
