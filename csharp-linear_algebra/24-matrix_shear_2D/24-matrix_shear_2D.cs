89866e249e5fa6eb9c959a151b2ac42f1333db58
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-01-27 23:21:51 +0100

    update

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,66 +1,35 @@
 using System;
 
-/// <summary>MatrixMath class</summary>
+/// <summary> 22. Matrix shear #1 </summary>
 class MatrixMath
 {
-    /// <summary>
-    /// Shears a square 2D matrix by a given shear factor and returns the resulting matrix.
-    /// </summary>
-    /// <param name="matrix">The square 2D matrix to shear.</param>
-    /// <param name="direction">The axis along which to shear ('x' or 'y').</param>
-    /// <param name="factor">The shear factor.</param>
-    /// <returns>
-    /// The resulting matrix after shear transformation, or a matrix containing -1 if invalid.
-    /// </returns>
+    /// <summary> method that shears a square 2D matrix by a given shear factor and returns the resulting matrix. </summary>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        // Vérifie si la matrice est 2x2
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        double[,] nmatrix = new double[2,2];
+        double[,] smatrix;
+        
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+        if (direction == 'x' || direction == 'y')
         {
-            return new double[,] { { -1 } };
+            if (direction == 'x')
+                smatrix = new double[,] {{1, 0}, {factor, 1}};
+            else
+                smatrix = new double[,] {{1, factor}, {0, 1}};
         }
-
-        // Vérifie si la direction est valide ('x' ou 'y')
-        if (direction != 'x' && direction != 'y')
-        {
-            return new double[,] { { -1 } };
-        }
-
-        // Crée la matrice de cisaillement
-        double[,] shearMatrix = new double[2, 2];
-        if (direction == 'x')
-        {
-            shearMatrix = new double[,]
-            {
-                { 1, factor },
-                { 0, 1 }
-            };
-        }
-        else if (direction == 'y')
-        {
-            shearMatrix = new double[,]
-            {
-                { 1, 0 },
-                { factor, 1 }
-            };
-        }
-
-        // Résultat
-        double[,] result = new double[2, 2];
-
-        // Applique la transformation
+        else
+            return new double[,] {{-1}};
         for (int i = 0; i < 2; i++)
-        {
+		{
             for (int j = 0; j < 2; j++)
-            {
-                result[i, j] = Math.Round(
-                    matrix[i, 0] * shearMatrix[0, j] +
-                    matrix[i, 1] * shearMatrix[1, j],
-                    2
-                );
+			{
+                for (int k = 0; k < 2; k++)
+				{
+                    nmatrix[i, j] += (matrix[i, k] * smatrix[k, j]);
+                }
             }
         }
-
-        return result;
+		return nmatrix;
     }
 }
