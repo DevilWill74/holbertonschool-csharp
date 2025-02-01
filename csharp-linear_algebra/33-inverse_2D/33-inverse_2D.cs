c4d26eb4d3a55d5d32784fc9d0eb7ae7166787b5
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-02-01 23:50:40 +0100

    test

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,36 +1,26 @@
 ﻿﻿using System;
-//I was lazy SOrry jajajaajajaj  There is an error in the main holbi
-/// /// <summary>Class matrix</summary>
+
+///<summary>MatrixMAth class</summary>
 class MatrixMath
 {
-    /// <summary>Inverse a matrix</summary>
-    public static double[,] Inverse(double[,] matrix)
+    ///<summary>method that calculates the inverse of a 2D matrix and returns the resulting matrix.</summary>
+    public static double[,] Inverse2D(double[,] matrix)
     {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        double[,] MT = {{-1}};
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
         
-
-        if (rows != 2)
-            return MT;
-        else
+        if (row != 2 && column != 2)
         {
-            if (matrix[0, 0] == 7)
-            {
-                double[,] matrix1 = { {0.15, -0.08}, {0.03, -0.18 } };
-                return matrix1;
-            }
-            if (matrix[0, 0] == 2)
-            {
-                double[,] matrix1 = { {0.5, 0},{-0.67, -0.17} };
-                return matrix1;
-            }
-            if (matrix[0, 0] == 3)
-            {
-                double[,] matrix1 =  { { 3, -3 }, { 1, -1 } };
-                return MT;
-            }
+            return new double[,] {{-1}};
         }
-        return MT;
+
+        double det = (matrix[0,0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        if (det == 0)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double[,] inverse = new double[,] {{Math.Round((1 / det) * matrix[1, 1], 2), Math.Round((1 / det) * -matrix[0, 1], 2)}, {Math.Round((1 / det) * -matrix[1, 0], 2), Math.Round((1 / det) * matrix[0, 0],2)}};
+        return inverse;
     }
 }
