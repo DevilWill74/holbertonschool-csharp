437d4453670c2b9ded3e3563681bd0771990e25d
DevilWill74 <alexis.fauqueur@outlook.fr>
2025-03-30 13:50:27 +0200

    test

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Player Class
+/// </summary>
+public class Player
+{
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Method Print
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name,this.hp,this.maxHp);
+    }
+}
---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+/// <summary>
+/// Player Class
+/// </summary>
+public class Player
+{
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Method Print
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name,this.hp,this.maxHp);
+    }
+
+    /// <summary>
+    /// Prints the damage taked.
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+            Console.WriteLine("{0} takes 0 damage!", this.name);
+        }
+        else
+        {
+            Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+        }
+    }
+
+    /// <summary>
+    /// Prints the hp points healed.
+    /// </summary>
+    /// <param name="heal"></param>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+            Console.WriteLine("{0} heals 0 HP!", this.name);
+        }
+        else
+        {
+            Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+        }
+    }
+}
+
+/// <summary>
+/// Delegate that takes a float amount.
+/// </summary>
+/// <param name="num"></param>
+public delegate void CalculateHealth(float num);
